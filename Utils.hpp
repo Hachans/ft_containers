540677a89d6097e930c602d0d66eda37ca541e86
Edgars Kraujins <ekraujin@c2s19d121.42wolfsburg.de>
2022-05-17 17:27:33 +0200

    Added std::pair

--- Utils.hpp
+++ Utils.hpp
@@ -45,6 +45,65 @@ bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
 	return (first1 == last1) && (first2 != last2);
 }
 
+
+
+template< class T1, class T2 > struct pair{
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		first_type first;
+		second_type second;
+
+		pair(): first(), second(){}
+		pair( const T1& x, const T2& y ): first(x), second(y){}
+		~pair(){}
+
+		template< class U1, class U2 >
+		pair( const pair<U1, U2>& p ) : first(p.first), second(p.second){}
+
+		pair& operator=( const pair& other ){
+			first = other.first;
+			second = other.second;
+			return *this;
+		}
+};
+
+template< class T1, class T2 >
+std::pair<T1,T2> make_pair( T1 t, T2 u ){
+	return(pair<T1, T2>(t, u));
+}
+
+template< class T1, class T2 >
+bool operator==( const pair<T1,T2>& lhs, const pair<T1,T2>& rhs ){
+	return (lhs.first == rhs.first && lhs.second = rhs.second);
+}
+
+template< class T1, class T2 >
+bool operator!=( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ){
+	return !(lhs == rhs);
+}
+
+template< class T1, class T2 >
+bool operator<( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ){
+	return (lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second));
+}
+
+template< class T1, class T2 >
+bool operator>( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ){
+	return !(rhs < lhs)
+}
+
+template< class T1, class T2 >
+bool operator<=( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ){
+	return rhs < lhs;
+}
+
+template< class T1, class T2 >
+bool operator>=( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ){
+	return !(lhs < rhs);
+}
+
+
 template< bool B, class T = void>
 struct enable_if {};
  
