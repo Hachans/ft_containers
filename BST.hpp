e0d898dd5d79acb828c94369addf91b735628300
Edgars Kraujins <ekraujin@c2s19d121.42wolfsburg.de>
2022-05-21 16:16:08 +0200

    small push

--- BST.hpp
+++ BST.hpp
@@ -12,14 +12,39 @@ template< class Key, class T, class Compare = std::less<Key>
 
 	public:
 
-		typedef T											value_type;
-		typedef Allocator									allocator_type;
+		typedef Key											key_type;
+		typedef T											mapped_type;
+		typedef ft::pair<const Key, T>						value_type;
 		typedef std::size_t									size_type;
 		typedef std::ptrdiff_t								difference_type;
+		typedef Compare										key_compare;
+		typedef Allocator									allocator_type;
 		typedef value_type&									reference;
 		typedef const value_type&							const_reference;
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
+
+		typedef struct node{
+			struct node *left;
+			struct node *right;
+			struct node *parent;
+			size_type data;
+		}node;
+
+	private:
+
+		node 			*_node;
+		allocator_type	_alloc;
+
+	public:
+
+		BST(const allocator_type& alloc = allocator_type()): _alloc(alloc){
+			_node->data = 0;
+			_node->left = NULL;
+			_node->right = NULL;
+			_node->parent = NULL;
+		}
+		~BST(){}
 
 };
 
--- map.hpp
+++ map.hpp
@@ -24,8 +24,8 @@ template< class Key, class T, class Compare = std::less<Key>
 		typedef const value_type&							const_reference;
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
-		typedef ft::BST<value_type>							iterator;
-		typedef ft::BST<value_type>							const_iterator;
+		typedef BST<value_type, key_compare>				iterator;
+		typedef BST<value_type, key_compare>				const_iterator;
 		typedef ft::reverse_iterator<iterator>				reverse_iterator;
 		typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
 
@@ -73,22 +73,22 @@ template< class Key, class T, class Compare = std::less<Key>
 
 		value_type& at( const Key& key );
 		const value_type& at( const Key& key ) const;
-		value_type& operator[]( const Key& key ){
-			if !key
-				insert key
+		// value_type& operator[]( const Key& key ){
+		// 	if !key
+		// 		insert key
 			
-		}
-		iterator begin();
-		const_iterator begin() const;
+		// }
+		// iterator begin();
+		// const_iterator begin() const;
 
-		iterator end();
-		const_iterator end() const;
+		// iterator end();
+		// const_iterator end() const;
 
-		reverse_iterator rbegin();
-		const_reverse_iterator rbegin() const;
+		// reverse_iterator rbegin();
+		// const_reverse_iterator rbegin() const;
 
-		reverse_iterator rend();
-		const_reverse_iterator rend() const;
+		// reverse_iterator rend();
+		// const_reverse_iterator rend() const;
 
 
 		bool empty() const {
@@ -103,29 +103,29 @@ template< class Key, class T, class Compare = std::less<Key>
 			return (std::numeric_limits<difference_type>::max());
 		}
 
-		void clear(){
-			for (; _size != 0; _size--)
-				erase();
-		}
+		// void clear(){
+		// 	for (; _size != 0; _size--)
+		// 		erase();
+		// }
 
-		std::pair<iterator, bool> insert( const value_type& value );
-		iterator insert( iterator hint, const value_type& value );
-		template< class InputIt > void insert( InputIt first, InputIt last );
-		void erase( iterator pos );
-		void erase( iterator first, iterator last );
-		size_type erase( const Key& key );
-		void swap( map& other );
-		size_type count( const Key& key ) const;
-		iterator find( const Key& key );
-		const_iterator find( const Key& key ) const;
-		std::pair<iterator,iterator> equal_range( const Key& key );
-		std::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
-		iterator lower_bound( const Key& key );
-		const_iterator lower_bound( const Key& key ) const;
-		iterator upper_bound( const Key& key );
-		const_iterator upper_bound( const Key& key ) const;
-		key_compare key_comp() const;
-		map::value_compare value_comp() const;
+		// std::pair<iterator, bool> insert( const value_type& value );
+		// iterator insert( iterator hint, const value_type& value );
+		// template< class InputIt > void insert( InputIt first, InputIt last );
+		// void erase( iterator pos );
+		// void erase( iterator first, iterator last );
+		// size_type erase( const Key& key );
+		// void swap( map& other );
+		// size_type count( const Key& key ) const;
+		// iterator find( const Key& key );
+		// const_iterator find( const Key& key ) const;
+		// std::pair<iterator,iterator> equal_range( const Key& key );
+		// std::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
+		// iterator lower_bound( const Key& key );
+		// const_iterator lower_bound( const Key& key ) const;
+		// iterator upper_bound( const Key& key );
+		// const_iterator upper_bound( const Key& key ) const;
+		// key_compare key_comp() const;
+		// map::value_compare value_comp() const;
 };
 
 template< class Key, class T, class Compare, class Alloc >
